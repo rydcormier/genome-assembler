e14e3224f9daf21b382a2dce1c5a10d9f32df66d
Ryan Cormier <rydcormier@gmail.com>
2020-07-11 14:34:09 -0400

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  genome-assembler
+//
+//  Author: Ryan Cormier <rydcormier@gmail.com>
+//  Date:   7/11/2020
+//
+#include <iostream>
+
+int main()
+{
+    // TODO: everything
+    return 0;
+}
